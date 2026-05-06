734e467470caa7c37181197a59682fc1aab7f3d5
Пользователь <777draft777@mail.ru>
2026-05-06 18:04:12 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@
 public class Main {
     public static void main(String[] args) {
         var dog=8.0;
-        System.out.println(dog);
         var cat=3.6;
-        System.out.println(cat);
         var paper=763789;
+        System.out.println(dog);
+        System.out.println(cat);
         System.out.println(paper);
 
         }
