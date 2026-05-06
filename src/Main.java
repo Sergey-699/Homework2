55feffdb2d79ea4860f787e6bf6e655946e11db0
Пользователь <777draft777@mail.ru>
2026-05-06 18:09:57 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+       System.out.println("Задание 1");
         var dog=8.0;
         var cat=3.6;
         var paper=763789;
@@ -10,5 +11,4 @@ public class Main {
         System.out.println(paper);
 
         }
-    }
 }
